04b9fd2e221846942834ed763df4ca97082cfeae
Valera <v.drozdovich@intelico.su>
2020-09-30 10:05:59 +0300

    feat: add all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length) return Math.min.apply(null, array);
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length) return Math.max.apply(null, array);
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length) {
+    let avg = 0
+    array.forEach((item) => {
+      avg += item / array.length 
+    })
+    return avg.toFixed(2)
+  } else return 0
 }
